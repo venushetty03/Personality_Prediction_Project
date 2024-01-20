e101274fef60dbe94d4f4bee4a5107015b41880b
venushetty03 <venushetty796@gmail.com>
2024-01-20 23:21:09 +0530

    test commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 app = Flask(__name__)
 model = joblib.load("train_model.pkl")
 scaler = StandardScaler()
-
+# comment
 @app.route('/')
 def home():
     return render_template('index.html')
